e2b4366f57a5491fcbd28c5e29c5536f3e67a913
Thomas Parker <32755663+tparker48@users.noreply.github.com>
2024-06-06 20:08:55 -0700

    update

--- example.py
+++ example.py
@@ -2,11 +2,11 @@ from codeweb.network_builder import NetworkBuilder
 from codeweb.import_parsing.styles.python_styles import python_import_styles
 from pyvis.network import Network
 
+
 if __name__ == '__main__':
     # Create a new Network with desired settings
     empty_network = Network(height="750px", width="100%", bgcolor="#222222", font_color="white", directed=True)
     empty_network.repulsion(node_distance=150)
-    
 
     # Scan project directory and populate network, export to html
     nb = NetworkBuilder(network=empty_network, import_styles=python_import_styles, ignore_external_imports=False)
